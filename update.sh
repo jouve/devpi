48fd04290cd303ba0d77fe8732b1c636e7791022
Cyril Jouve <jv.cyril@gmail.com>
2020-04-25 16:13:33 +0200

    bump alpine

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if ! test -w /var/run/docker.sock; then
   SUDO=sudo
 else
   SUDO=
 fi
-$SUDO docker run -it -v $PWD:/srv -w /srv alpine:3.11 sh -c "
+$SUDO docker run -it -v $PWD:/srv -w /srv $(sed -n -e 's/FROM //p' Dockerfile) sh -c "
 set -e
 apk add --no-cache gcc libffi libffi-dev musl-dev python3 python3-dev
 pip3 install --upgrade pip
